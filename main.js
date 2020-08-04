8fe50f13319dd7de4e57718a595df1a3cb0a1c1c
Dylan Marcus <dylan@mach1studios.com>
2020-08-04 14:35:52 -0400

    js: init for handling security seed

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 });
 app.use('/public', express.static(__dirname + '/public'));
 
-var udp_server = dgram.createSocket('udp4', function(msg, rinfo) {
+var osc_udp_server = dgram.createSocket('udp4', function(msg, rinfo) {
   var osc_message;
   try {
     osc_message = osc.fromBuffer(msg);
@@ -37,8 +37,35 @@ var udp_server = dgram.createSocket('udp4', function(msg, rinfo) {
 
 });
 
-udp_server.bind(9998);
+var security_udp_server = dgram.createSocket('udp4', function(msg, rinfo) {
+  var msg;
+  try {
+    security_udp_server.on('message', (msg, rinfo) => {
+      console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    });
+    console.log(msg);
+  } catch(err) {
+    return console.log('Could not decode Security Seed');
+  }
+
+  // store the remote ip receiving OSC
+  remote_osc_ip = rinfo.address;
+
+  // pipe collected message to client's browser via new socket
+  io.emit('seed', {
+    x: parseString(msg.args[0].value) || 0
+  });
+  io.emit('internal', {
+    x: parseString(msg.args[0].value) || 0
+  });
+
+});
+
+osc_udp_server.bind(9998);
 console.log('Listening for OSC messages on port 9998');
+
+security_udp_server.bind(9995);
+console.log('Listening for OSC messages on port 9995');
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
